863f1b49df563560bcf8f688ea61ac7d23ed7cca
M.Herzog <bytestream93@gmail.com>
2022-12-13 10:07:11 +0100

    started day5 first part

---
+++ Day5/Day5.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    public class Day5
+    {
+
+        /*
+
+        [N]     [C]                 [Q]    
+        [W]     [J] [L]             [J] [V]
+        [F]     [N] [D]     [L]     [S] [W]
+        [R] [S] [F] [G]     [R]     [V] [Z]
+        [Z] [G] [Q] [C]     [W] [C] [F] [G]
+        [S] [Q] [V] [P] [S] [F] [D] [R] [S]
+        [M] [P] [R] [Z] [P] [D] [N] [N] [M]
+        [D] [W] [W] [F] [T] [H] [Z] [W] [R]
+         1   2   3   4   5   6   7   8   9 
+
+        */
+       
+
+        public static void Main(string[] args)
+        {
+
+           
+
+
+            part1();
+
+        }
+
+
+
+            static void part1()
+            {
+            var stack = new List<string>[9] {
+
+            new List<string>() { "D", "M", "S", "Z", "R", "F", "W", "N" },
+            new List<string>() { "W", "P", "Q", "G", "S" },
+            new List<string>() { "W", "R", "V", "Q", "F", "N", "J", "C" },
+            new List<string>() { "F", "Z", "P", "C", "G", "D", "L" },
+            new List<string>() { "T", "P", "S" },
+            new List<string>() { "H", "D", "F", "W", "R", "L" },
+            new List<string>() { "Z", "N", "D", "C" },
+            new List<string>() { "W", "N", "R", "F", "V", "S", "J", "Q" },
+            new List<string>() { "R", "M", "S", "G", "Z", "W", "V" },
+        };
+
+
+            var moves = File.ReadAllLines("C:\\Users\\herzo\\source\\repos\\ByteStream93\\AdventOfCode-2022\\AdventOfCode-2022\\Day5\\Input5.txt");
+
+            foreach (var move in moves)
+            {
+                var numbers = move.Split("-");
+
+                var amount = Int32.Parse(numbers[1]);
+                var from = Int32.Parse(numbers[2]);
+                var to = Int32.Parse(numbers[3]);
+
+                var actual = stack[from];
+
+                for (int i = 1; i <= amount; i++)
+                {
+                    
+                    var piece = actual[actual.Count - 1];
+                    stack[to].Add(piece);
+                    actual.RemoveAt(actual.Count - 1);
+                }
+            }
+
+                string result = "";
+
+                foreach(var place in stack)
+                {
+                result += place[place.Count - 1];
+
+                }
+            Console.WriteLine(result);
+
+
+            }
+
+
+
+
+            }
+        }
+    
+
+
+
+    
+
