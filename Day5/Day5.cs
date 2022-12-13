6fa768953a7bc2c1f60249a34bbfe1cf7da397ad
M.Herzog <bytestream93@gmail.com>
2022-12-13 15:36:18 +0100

    Finished Day5

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -23,22 +23,25 @@ namespace Day5
          1   2   3   4   5   6   7   8   9 
 
         */
-       
+
 
         public static void Main(string[] args)
         {
 
-           
 
 
+            Console.WriteLine("First part of day 5: ");
             part1();
 
-        }
+            Console.WriteLine();
 
+            Console.WriteLine("Second part of day 5: ");
+            part2();
 
+        }     
 
-            static void part1()
-            {
+        static void part1()
+        {
             var stack = new List<string>[9] {
 
             new List<string>() { "D", "M", "S", "Z", "R", "F", "W", "N" },
@@ -60,39 +63,95 @@ namespace Day5
                 var numbers = move.Split("-");
 
                 int amount = Int32.Parse(numbers[1]);
-                int from = (Int32.Parse(numbers[2]))-1;
-                int to = Int32.Parse((numbers[3]))-1;
-                
-               
+                int from = (Int32.Parse(numbers[2])) - 1;
+                int to = Int32.Parse((numbers[3])) - 1;
+
+
                 var actual = stack[from];
-                
+
                 for (int i = 1; i <= amount; i++)
                 {
-                    
+
                     var piece = actual[actual.Count - 1];
                     stack[to].Add(piece);
                     actual.RemoveAt(actual.Count - 1);
                 }
             }
 
-                string result = "";
+            string result = "";
 
-                foreach(var place in stack)
-                {
+            foreach (var place in stack)
+            {
                 result += place[place.Count - 1];
 
-                }
+            }
             Console.WriteLine(result);
 
 
-            }
-
-
-
-
-            }
         }
-    
+
+        static void part2()
+        {
+            var stack = new List<string>[9] {
+
+            new List<string>() { "D", "M", "S", "Z", "R", "F", "W", "N" },
+            new List<string>() { "W", "P", "Q", "G", "S" },
+            new List<string>() { "W", "R", "V", "Q", "F", "N", "J", "C" },
+            new List<string>() { "F", "Z", "P", "C", "G", "D", "L" },
+            new List<string>() { "T", "P", "S" },
+            new List<string>() { "H", "D", "F", "W", "R", "L" },
+            new List<string>() { "Z", "N", "D", "C" },
+            new List<string>() { "W", "N", "R", "F", "V", "S", "J", "Q" },
+            new List<string>() { "R", "M", "S", "G", "Z", "W", "V" },
+        };
+            
+
+            var moves = File.ReadAllLines("C:\\Users\\herzo\\source\\repos\\ByteStream93\\AdventOfCode-2022\\AdventOfCode-2022\\Day5\\Input5.txt");
+
+            foreach (var move in moves)
+            {
+                var numbers = move.Split("-");
+
+                var amount = Int32.Parse(numbers[1]);
+                var from = (Int32.Parse(numbers[2])) - 1;
+                var to = Int32.Parse((numbers[3])) - 1;
+
+
+                var actual = stack[from];
+
+                var newList = new List<string>();
+
+                for (int i = 1; i <= amount; i++)
+                {
+                    var piece = actual[actual.Count - 1];
+                    newList.Add(piece);
+                    actual.RemoveAt(actual.Count - 1);
+                    Console.WriteLine(piece);
+                }
+
+                newList.Reverse();
+
+                foreach (var piece in newList)
+                {
+                    stack[to].Add(piece);
+
+                }
+
+            }
+
+            string result = "";
+
+            foreach (var place in stack)
+            {
+                result += place[place.Count - 1];
+
+            }
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
 
 
 
