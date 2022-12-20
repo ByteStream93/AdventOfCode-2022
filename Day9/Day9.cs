4b68e3244abef86dbc9ed420553acf19f151c149
Marco <bytestream93@gmail.com>
2022-12-20 15:12:33 +0100

    day9 rope moving correctly but HashSet got the wrong count

--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -17,6 +17,9 @@ namespace Day9
 
             Console.WriteLine();
 
+            Console.WriteLine("Second part of day 9: ");
+            part2();
+
         }
 
         static void part1()
@@ -94,6 +97,100 @@ namespace Day9
 
         }
 
+        static void part2()
+        {
+            var inputText = File.ReadAllLines("C:\\Users\\herzo\\Source\\Repos\\ByteStream93\\AdventOfCode-2022\\Day9\\TextFile1.txt");
+
+            var input = new List<string>(inputText);
+
+            var positions = new HashSet<string>();
+
+
+           int head_x = 0;
+           int head_y = 0;
+
+            var tail_x = new int[9];
+            var tail_y = new int[9];
+
+
+
+
+            foreach (var line in input)
+            {
+                var direction = line.Split(" ");
+
+                for (int move = 0; move < Int32.Parse(direction[1]); move++)
+                {
+
+
+                    //Console.WriteLine(direction[0]);
+
+                    if (direction[0] == "U")
+                    {
+                        head_y--;
+                    }
+                    else if (direction[0] == "D")
+                    {
+                        head_y++;
+                    }
+                    else if (direction[0] == "L")
+                    {
+                        head_x--;
+                    }
+                    else if (direction[0] == "R")
+                    {
+                        head_x++;
+                    }
+
+                    if (!(Math.Abs(head_x - tail_x[0]) <= 1) || !(Math.Abs(head_y - tail_y[0]) <= 1))
+                    {
+
+                        if (head_x > tail_x[0])
+                        {
+                            tail_x[0] += 1;
+                        }
+                        else if (head_x < tail_x[0])
+                        {
+                            tail_x[0] -= 1;
+                        }
+                        if (head_y > tail_y[0])
+                        {
+                            tail_y[0] += 1;
+                        }
+                        else if (head_y < tail_y[0])
+                        {
+                            tail_y[0] -= 1;
+                        }
+
+                    }
+
+                    for (int tailPosition = tail_x.Length-1; tailPosition > 0; tailPosition--) {
+                                             
+                        
+                            if (tail_x[tailPosition] != tail_x[tailPosition - 1] || tail_y[tailPosition] != tail_y[tailPosition - 1])
+                            {
+                                tail_x[tailPosition] = tail_x[tailPosition - 1];
+                                tail_y[tailPosition] = tail_y[tailPosition - 1];
+                            }
+                        
+                        Console.WriteLine("TailIndex:" + tailPosition + " x:" + tail_x[tailPosition] + " y:" + tail_y[tailPosition]);
+                        if (tailPosition == 8)
+                        {
+                            Console.WriteLine();
+                        }
+                        
+                        positions.Add(addPosition(tail_x[8], tail_y[8]));
+                    }
+                    
+                    
+
+                }
+
+            }
+            Console.WriteLine(positions.Count);
+        }
+
+
         static string addPosition(int x_coordinate, int y_coordinate)
         {
 
@@ -101,6 +198,7 @@ namespace Day9
 
         }
 
+      
     
         }
 
